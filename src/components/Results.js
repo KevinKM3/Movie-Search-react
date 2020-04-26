62d04b5b3937ee074fcf0f6a22aed47174b9fbab
Kevin Matthews <kevmatthews3@gmail.com>
2020-04-25 21:02:40 -0400

    css-results

--- src/components/Results.js
+++ src/components/Results.js
@@ -6,7 +6,7 @@ function Results({ results }) {
   return (
     <section className="results">
       {results.map((result) => (
-        <Result result={result} />
+        <Result key={result.imdbID} result={result} />
       ))}
     </section>
   );
